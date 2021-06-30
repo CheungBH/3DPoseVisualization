c2bcc8fdb64ea7443d303ebce24f71f63479cfbb
cheungbh <534660436@qq.com>
2021-06-30 11:57:49 +0800

    add matching to images

--- move_with_angle_17kps.py
+++ move_with_angle_17kps.py
@@ -9,8 +9,8 @@ root = "resources/constraints"
 static_pose_path = os.path.join(root, "staticPose.npy")
 static_pose = np.load(static_pose_path, allow_pickle=True).item()
 # indices used for computing bone vectors for non-torso bones
-nt_parent_indices = [13, 17, 18, 25, 26, 6, 7, 1, 2]
-nt_child_indices = [15, 18, 19, 26, 27, 7, 8, 2, 3]
+nt_parent_indices = [8, 11, 12, 14, 15, 4, 5, 1, 2]
+nt_child_indices = [10, 12, 13, 15, 16, 5, 6, 2, 3]
 di_indices = {2:5, 4:2, 6:13, 8:9}
 di = static_pose['di']
 a = static_pose['a'].reshape(3)
@@ -61,12 +61,12 @@ def get_basis1(skeleton):
     This system is used for upper-limbs.
     """
     # compute the vector from the left shoulder to the right shoulder
-    left_shoulder = skeleton[17]
-    right_shoulder = skeleton[25]
+    left_shoulder = skeleton[11]
+    right_shoulder = skeleton[14]
     v1 = normalize(right_shoulder - left_shoulder)
     # compute the backbone vector from the thorax to the spine
-    thorax = skeleton[13]
-    spine = skeleton[12]
+    thorax = skeleton[8]
+    spine = skeleton[7]
     v2 = normalize(spine - thorax)
     # v3 is the cross product of v1 and v2 (front-facing vector for upper-body)
     v3 = normalize(np.cross(v1, v2))
@@ -113,7 +113,7 @@ def to_local(skeleton):
     """
     v1, v2, v3 = get_basis1(skeleton)
     # compute the vector from the left hip to the right hip
-    left_hip = skeleton[6]
+    left_hip = skeleton[4]
     right_hip = skeleton[1]
     v4 = normalize(right_hip - left_hip)
     # v5 is the cross product of v4 and v2 (front-facing vector for lower-body)
@@ -207,7 +207,7 @@ def re_order(skeleton):
     return skeleton[:, [0,2,1]]
 
 
-def show3Dpose(channels,
+def show3Dpose(joints,
                ax,
                lcolor="#3498db",
                rcolor="#e74c3c",
@@ -229,36 +229,19 @@ def show3Dpose(channels,
       Nothing. Draws on ax.
     """
 
-    if channels.shape[0] == 96:
-        vals = np.reshape(channels, (32, -1))
-    else:
-        vals = channels
-    I = np.array([1, 2, 3, 1, 7, 8, 1, 13, 14, 15, 14, 18, 19, 14, 26, 27]) - 1  # start points
-    J = np.array([2, 3, 4, 7, 8, 9, 13, 14, 15, 16, 18, 19, 20, 26, 27, 28]) - 1  # end points
-    LR = np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], dtype=bool)
-    dim_use_3d = [3, 4, 5, 6, 7, 8, 9, 10, 11, 18, 19, 20, 21, 22, 23, 24, 25,
-                  26, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 51, 52, 53,
-                  54, 55, 56, 57, 58, 59, 75, 76, 77, 78, 79, 80, 81, 82, 83]
+    I = np.array([0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15]) # start points
+    J = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])  # end points
+    LR = np.array([0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], dtype=bool)
+
     # Make connection matrix
     for i in np.arange(len(I)):
-        x, y, z = [np.array([vals[I[i], j], vals[J[i], j]]) for j in range(3)]
-        if gt:
-            ax.plot(x, y, z, lw=4, c='k')
-        #        ax.plot(x,y, z,  lw=2, c='k')
-
-        elif pred:
-            ax.plot(x, z, -y, lw=4, c='r')
-        #        ax.plot(x,y, z,  lw=2, c='r')
-
-        else:
-            #        ax.plot(x,z, -y,  lw=2, c=lcolor if LR[i] else rcolor)
-            ax.plot(x, y, z, lw=4, c=lcolor if LR[i] else rcolor)
+        x, y, z = [np.array([joints[I[i], j], joints[J[i], j]]) for j in range(3)]
+        ax.plot(x, y, z, lw=4, c=lcolor if LR[i] else rcolor)
     if plot_dot:
-        joints = channels.reshape(96)
-        joints = joints[dim_use_3d].reshape(16, 3)
         ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], c='k', marker='o')
+
     RADIUS = 750  # space around the subject
-    xroot, yroot, zroot = vals[0, 0], vals[0, 1], vals[0, 2]
+    xroot, yroot, zroot = joints[0, 0], joints[0, 1], joints[0, 2]
     ax.set_xlim3d([-RADIUS + xroot, RADIUS + xroot])
     ax.set_zlim3d([-RADIUS + zroot, RADIUS + zroot])
     ax.set_ylim3d([-RADIUS + yroot, RADIUS + yroot])
@@ -347,7 +330,7 @@ def to_global(skeleton, bones_local, cache=False):
     return_value = {}
     v1, v2, v3 = get_basis1(skeleton)
     # compute the vector from the left hip to the right hip
-    left_hip = skeleton[6]
+    left_hip = skeleton[4]
     right_hip = skeleton[1]
     v4 = normalize(right_hip - left_hip)
     # v5 is the cross product of v4 and v2 (front-facing vector for lower-body)
@@ -410,13 +393,13 @@ def set_z(pose, target):
     if pose is None:
         return None
     original_shape = pose.shape
-    pose = pose.reshape(32, 3)
+    pose = pose.reshape(17, 3)
     min_val = pose[:, 2].min()
     pose[:, 2] -= min_val - target
     return pose.reshape(original_shape)
 
 
-total_joints_num = 32
+total_joints_num = 17
 
 
 def exploration(father, angles_dict):
@@ -449,34 +432,45 @@ def exploration(father, angles_dict):
     # plt.title('mother')
     ax3 = plt.subplot(2, 1, 2, projection='3d')
     # plt.title('son: ' + str(valid_vec_fa.sum()))
-    show3Dpose(modified_pose, ax3, add_labels=False, plot_dot=True)
+    show3Dpose(modified_pose.reshape(total_joints_num, -1), ax3, add_labels=False, plot_dot=True)
 
     plt.tight_layout()
-    plt.savefig("./EXAMPLE1.JPG")
+    plt.savefig("./EXAMPLE17.JPG")
     plt.clf()
     return modified_pose
+
+
+def get_useful_idx(ls):
+    useful = []
+    for item in ls:
+        useful.append(item*3)
+        useful.append(item*3+1)
+        useful.append(item*3+2)
+    return useful
 
 
 if __name__ == '__main__':
     import cv2
     target_frame_num = 500
     maximum_angle = 10
-    useful_idx = [0,1,2,3,6,7,8,12,13,14,15,17,18,19,25,26,27]
+    useful_idx = get_useful_idx([0,1,2,3,6,7,8,12,13,14,15,17,18,19,25,26,27])
     plt.figure()
     out = cv2.VideoWriter('angle_random.mp4', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (900, 900))
-    # angle_dicts = {1: [10, 10, 10],
-    #                # 2: [5, 15, 10],
-    #                8: [20, 30, 20]}
+    angle_dicts = {1: [10, 10, 10],
+                   # 2: [5, 15, 10],
+                   8: [20, 30, 20]}
     bone = np.array([-26.017000198364258, 659.6329956054688, 917.6480102539062, -130.90054321289062, 603.06494140625, 923.584228515625, -162.97560119628906, 484.32232666015625, 513.33984375, -276.5113220214844, 538.9822387695312, 89.21865844726562, -264.859130859375, 403.36260986328125, 34.39588928222656, -265.51898193359375, 332.306640625, 58.38550567626953, 78.86579895019531, 716.20068359375, 911.7118530273438, 109.70648193359375, 728.9241943359375, 484.7274475097656, 69.05551147460938, 862.8753051757812, 65.01152038574219, 150.8942108154297, 745.80712890625, 31.378429412841797, 200.72909545898438, 694.290283203125, 53.457279205322266, -26.011560440063477, 659.6333618164062, 917.7478637695312, -53.7849235534668, 659.4111328125, 1140.2408447265625, -88.10198211669922, 626.5931396484375, 1389.8197021484375, -66.125, 532.8142700195312, 1456.4462890625, -112.96037292480469, 602.103515625, 1535.3773193359375, -88.10198211669922, 626.5931396484375, 1389.8197021484375, 20.55335807800293, 709.119873046875, 1346.6932373046875, 241.72450256347656, 752.8900146484375, 1208.22802734375, 392.4771728515625, 560.609130859375, 1254.20361328125, 392.4771728515625, 560.609130859375, 1254.20361328125, 345.77362060546875, 546.5153198242188, 1341.496337890625, 475.9122009277344, 520.8990478515625, 1292.4317626953125, 475.9122009277344, 520.8990478515625, 1292.4317626953125, -88.10198211669922, 626.5931396484375, 1389.8197021484375, -187.40086364746094, 567.8629760742188, 1305.1588134765625, -151.79685974121094, 424.5013427734375, 1085.6495361328125, 78.09835815429688, 508.656982421875, 1128.986572265625, 78.09835815429688, 508.656982421875, 1128.986572265625, 77.14738464355469, 587.83203125, 1190.065185546875, 206.67327880859375, 479.8706970214844, 1168.303466796875, 206.67327880859375, 479.8706970214844, 1168.303466796875])
-    #
-    # exploration(bone, angle_dicts)
+    bone = bone[useful_idx]
 
-    for i in range(target_frame_num):
-        random_angles_dict = {i: [np.random.normal(scale=maximum_angle), np.random.normal(scale=maximum_angle),
-                                  np.random.normal(scale=maximum_angle)] for i in range(len(root_joints))}
-        bone = exploration(bone, random_angles_dict)
-        frame = cv2.resize(cv2.imread("./EXAMPLE1.JPG"), (900, 900))
-        cv2.imshow("mutation", frame)
-        out.write(frame)
-        cv2.waitKey(1)
-    out.release()
+    exploration(bone, angle_dicts)
+
+    #
+    # for i in range(target_frame_num):
+    #     random_angles_dict = {i: [np.random.normal(scale=maximum_angle), np.random.normal(scale=maximum_angle),
+    #                               np.random.normal(scale=maximum_angle)] for i in range(len(root_joints))}
+    #     bone = exploration(bone, random_angles_dict)
+    #     frame = cv2.resize(cv2.imread("./EXAMPLE1.JPG"), (900, 900))
+    #     cv2.imshow("mutation", frame)
+    #     out.write(frame)
+    #     cv2.waitKey(1)
+    # out.release()
---
+++ video_with_kps.py
@@ -0,0 +1,415 @@
+#-*-coding:utf-8-*-
+#-*-coding:utf-8-*-
+import cv2
+import numpy as np
+import mv_utils.transformations as transformations
+import matplotlib.pyplot as plt
+import torch
+
+
+def project_3d_points_to_image_plane_without_distortion(proj_matrix, points_3d, convert_back_to_euclidean=True):
+    """Project 3D points to image plane not taking into account distortion
+    Args:
+        proj_matrix numpy array or torch tensor of shape (3, 4): projection matrix
+        points_3d numpy array or torch tensor of shape (N, 3): 3D points
+        convert_back_to_euclidean bool: if True, then resulting points will be converted to euclidean coordinates
+                                        NOTE: division by zero can be here if z = 0
+    Returns:
+        numpy array or torch tensor of shape (N, 2): 3D points projected to image plane
+    """
+    if isinstance(proj_matrix, np.ndarray) and isinstance(points_3d, np.ndarray):
+        result = euclidean_to_homogeneous(points_3d) @ proj_matrix.T
+        if convert_back_to_euclidean:
+            result = homogeneous_to_euclidean(result)
+        return result
+    elif torch.is_tensor(proj_matrix) and torch.is_tensor(points_3d):
+        result = euclidean_to_homogeneous(points_3d) @ proj_matrix.t()
+        if convert_back_to_euclidean:
+            result = homogeneous_to_euclidean(result)
+        return result
+    else:
+        raise TypeError("Works only with numpy arrays and PyTorch tensors.")
+
+
+class Camera:
+    def __init__(self, R, t, K, dist=None, name=""):
+        self.R = R.copy()
+        self.t = t.copy()
+        self.K = K.copy()
+        self.dist = dist
+
+        self.name = name
+
+    def update_after_crop(self, bbox):
+        left, upper, right, lower = bbox
+
+        cx, cy = self.K[0, 2], self.K[1, 2]
+
+        new_cx = cx - left
+        new_cy = cy - upper
+
+        self.K[0, 2], self.K[1, 2] = new_cx, new_cy
+
+    def update_after_resize(self, image_shape, new_image_shape):
+        height, width = image_shape
+        new_width, new_height = new_image_shape
+
+        fx, fy, cx, cy = self.K[0, 0], self.K[1, 1], self.K[0, 2], self.K[1, 2]
+
+        new_fx = fx * (new_width / width)
+        new_fy = fy * (new_height / height)
+        new_cx = cx * (new_width / width)
+        new_cy = cy * (new_height / height)
+
+        self.K[0, 0], self.K[1, 1], self.K[0, 2], self.K[1, 2] = new_fx, new_fy, new_cx, new_cy
+
+    @property
+    def projection(self):
+        return self.K.dot(self.extrinsics)
+
+    @property
+    def extrinsics(self):
+        return np.hstack([self.R, self.t])
+
+
+def generate_grid_mesh(start, end, step=1.0):
+    num_point_per_line = int((end - start) // step + 1)
+    its = np.linspace(start, end, num_point_per_line)
+    line = []
+    color = []
+    common_line_color = [192, 192, 192]
+    for i in range(num_point_per_line):
+        line.append([its[0], its[i], 0, its[-1], its[i], 0])
+        if its[i] == 0:
+            color.append([0, 255, 0])
+        else:
+            color.append(common_line_color)
+
+    for i in range(num_point_per_line):
+        line.append([its[i], its[-1], 0, its[i], its[0], 0])
+        if its[i] == 0:
+            color.append([0, 0, 255])
+        else:
+            color.append(common_line_color)
+
+    return np.array(line, dtype=np.float32), np.array(color, dtype=np.uint8)
+
+
+def euclidean_to_homogeneous(points):
+    if isinstance(points, np.ndarray):
+        return np.hstack([points, np.ones((len(points), 1))])
+    else:
+        raise TypeError("Works only with numpy arrays")
+
+
+def homogeneous_to_euclidean(points):
+    if isinstance(points, np.ndarray):
+        return (points.T[:-1] / points.T[-1]).T
+    else:
+        raise TypeError("Works only with numpy arrays")
+
+
+def projection_to_2d_plane(vertices, projection_matrix, view_matrix=None, scale=None):
+    if view_matrix is not None:
+        vertices = (homogeneous_to_euclidean(
+            (euclidean_to_homogeneous(vertices) @ view_matrix.T) @ projection_matrix.T)[:, :2]) * scale
+
+        vertices[:, 1] = scale - vertices[:, 1]
+        vertices[:, 0] = vertices[:, 0] + scale
+    else:
+        vertices = euclidean_to_homogeneous(vertices) @ projection_matrix.T
+        vertices = homogeneous_to_euclidean(vertices)
+    return vertices.astype(np.int32)
+
+
+def look_at(eye, center, up):
+    f = unit_vector(center - eye)
+    u = unit_vector(up)
+    s = unit_vector(np.cross(f, u))
+    u = np.cross(s, f)
+
+    result = transformations.identity_matrix()
+    result[:3, 0] = s
+    result[:3, 1] = u
+    result[:3, 2] = -f
+    result[3, 0] = -np.dot(s, eye)
+    result[3, 1] = -np.dot(u, eye)
+    result[3, 2] = np.dot(f, eye)
+    return result.T
+
+
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+
+def update_camera_vectors():
+    global front
+    front_temp = np.zeros((3,))
+    front_temp[0] = np.cos(np.radians(yaw)) * np.cos(np.radians(pitch))
+    front_temp[1] = np.sin(np.radians(pitch))
+    front_temp[2] = np.sin(np.radians(yaw)) * np.cos(np.radians(pitch))
+    front = unit_vector(front_temp)
+    global right
+    right = unit_vector(np.cross(front, world_up))
+
+
+def show3Dpose(joints,
+               ax,
+               lcolor="#3498db",
+               rcolor="#e74c3c",
+               add_labels=True,
+               gt=False,
+               pred=False,
+               plot_dot=False
+               ):  # blue, orange
+    """
+    Visualize a 3d skeleton
+
+    Args
+      channels: 96x1 vector. The pose to plot.
+      ax: matplotlib 3d axis to draw on
+      lcolor: color for left part of the body
+      rcolor: color for right part of the body
+      add_labels: whether to add coordinate labels
+    Returns
+      Nothing. Draws on ax.
+    """
+
+    I = np.array([0, 1, 2, 5, 4, 3, 6, 7, 8, 9, 8, 11, 10, 8, 13, 14]) # start points
+    J = np.array([1, 2, 6, 4, 3, 6, 7, 8, 16, 16, 12, 12, 11, 13, 14, 15])  # end points
+    LR = np.array([0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], dtype=bool)
+
+    # Make connection matrix
+    for i in np.arange(len(I)):
+        x, y, z = [np.array([joints[I[i], j], joints[J[i], j]]) for j in range(3)]
+        ax.plot(x, y, z, lw=4, c=lcolor if LR[i] else rcolor)
+    if plot_dot:
+        ax.scatter(joints[:, 0], joints[:, 1], joints[:, 2], c='k', marker='o')
+
+    RADIUS = 750  # space around the subject
+    xroot, yroot, zroot = joints[2, 0], joints[2, 1], joints[2, 2]
+    ax.set_xlim3d([-RADIUS + xroot, RADIUS + xroot])
+    ax.set_zlim3d([-RADIUS + zroot, RADIUS + zroot])
+    ax.set_ylim3d([-RADIUS + yroot, RADIUS + yroot])
+
+    if add_labels:
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
+    ax.set_aspect('auto')
+    #  ax.set_xticks([])
+    #  ax.set_yticks([])
+    #  ax.set_zticks([])
+
+    #  ax.get_xaxis().set_ticklabels([])
+    #  ax.get_yaxis().set_ticklabels([])
+    #  ax.set_zticklabels([])
+    # Get rid of the panes (actually, make them white)
+    #  white = (1.0, 1.0, 1.0, 0.0)
+    #  ax.w_xaxis.set_pane_color(white)
+    #  ax.w_yaxis.set_pane_color(white)
+    # Keep z pane
+
+    # Get rid of the lines in 3d
+    #  ax.w_xaxis.line.set_color(white)
+    #  ax.w_yaxis.line.set_color(white)
+    #  ax.w_zaxis.line.set_color(white)
+    ax.view_init(10, -60)
+
+import os
+
+camera_vertices = np.array([[0, 0, 0], [-1, -1, 2],
+                            [0, 0, 0], [1, 1, 2],
+                            [0, 0, 0], [1, -1, 2],
+                            [0, 0, 0], [-1, 1, 2],
+                            [-1, 1, 2], [-1, -1, 2],
+                            [-1, -1, 2], [1, -1, 2],
+                            [1, -1, 2], [1, 1, 2],
+                            [1, 1, 2], [-1, 1, 2]], dtype=np.float32)
+
+human36m_connectivity_dict = [(0, 1), (1, 2), (2, 6), (5, 4), (4, 3), (3, 6), (6, 7), (7, 8), (8, 16), (9, 16), (8, 12),
+                              (11, 12), (10, 11), (8, 13), (13, 14), (14, 15)]
+
+multiview_data = np.load("resources/3D_sence_multiview.npy", allow_pickle=True).tolist()
+subject_name, camera_name, action_name, camera_configs, labels = multiview_data['subject_names'], multiview_data[
+    'camera_names'], multiview_data['action_names'], multiview_data['cameras'], multiview_data['table']
+
+camera_name = [str(i) for i, c in enumerate(camera_name)]
+
+# subject_name ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
+
+# video_action_name = ['Directions 1', 'Directions', 'Discussion 1', 'Discussion 2', 'Eating', 'Eating 1', 'Greeting 1', 'Greeting 2', 'Phoning', 'Phoning 1', 'Posing', 'Posing 1', 'Purchases', 'Purchases 1', 'Sitting 1', 'Sitting', 'SittingDown', 'SittingDown 1', 'Smoking', 'Smoking 1', 'Photo', 'Photo 2', 'Waiting 1', 'Waiting 2', 'Walking', 'Walking 1', 'WalkDog', 'WalkDog 1', 'WalkingTogether', 'WalkingTogether 1']
+label_action_name = ['Directions-1', 'Directions-2', 'Discussion-1', 'Discussion-2', 'Eating-1', 'Eating-2', 'Greeting-1', 'Greeting-2', 'Phoning-1', 'Phoning-2', 'Posing-1', 'Posing-2', 'Purchases-1', 'Purchases-2', 'Sitting-1', 'Sitting-2', 'SittingDown-1', 'SittingDown-2', 'Smoking-1', 'Smoking-2', 'TakingPhoto-1', 'TakingPhoto-2', 'Waiting-1', 'Waiting-2', 'Walking-1', 'Walking-2', 'WalkingDog-1', 'WalkingDog-2', 'WalkingTogether-1', 'WalkingTogether-2']
+
+out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (900, 900))
+img_root = "/media/hkuit155/Windows/1TB_dataset/multi_view/h36m/images"
+specific_subject = "S1"
+specific_action = "Directions-1"
+mask_subject = labels['subject_idx'] == subject_name.index(specific_subject)
+actions = [action_name.index(specific_action)]
+# img_folder = os.path.join(img_root, "processed", specific_subject, specific_action, "imageSequence-undistorted")
+
+mask_actions = np.isin(labels['action_idx'], actions)
+mask_subject = mask_subject & mask_actions
+indices = []
+indices.append(np.nonzero(mask_subject)[0])
+specific_label = labels[np.concatenate(indices)]
+specific_3d_skeleton = specific_label['keypoints']
+
+specific_camera_config = camera_configs[subject_name.index(specific_subject)]
+specific_camera_config = [
+    Camera(specific_camera_config["R"][i], specific_camera_config["t"][i], specific_camera_config["K"][i]) for i in
+    range(len(camera_name))]
+
+# first person setup
+yaw = -125
+pitch = -15
+world_up = np.array([0.0, 1.0, 0.0])
+position = np.array([5000, 2500, 7557])
+front = np.array([0.0, 0.0, -1.0])
+right = np.array([0.0, 0.0, 0.0])
+
+grid_vertices, grid_color = generate_grid_mesh(-4, 4, step=1)
+grid_vertices = grid_vertices.reshape(-1, 3)
+
+rorate_x_90 = transformations.rotation_matrix(np.radians(-90), (1, 0, 0))
+
+frame_size = 900
+original_video_frame_size = 1000
+frame = np.zeros([frame_size, frame_size])
+
+for i in range(len(camera_name)):
+    specific_camera_config[i].update_after_resize((original_video_frame_size,) * 2,
+                                                  (frame_size,) * 2)
+
+update_camera_vectors()
+view_matrix = look_at(position, position + front, world_up)
+
+projection_matrix = np.array([[2.41421, 0, 0, 0],
+                              [0, 2.41421, 0, 0],
+                              [0, 0, -1, -0.2],
+                              [0, 0, -1, 0]], dtype=np.float32)
+
+o_view_matrix = view_matrix.copy()
+o_projection_matrix = projection_matrix.copy()
+
+total_frame = specific_3d_skeleton.shape[0]
+frame_index = 0
+
+view_camera_index = 3
+plt.figure()
+# cam_dict = {0: "54138969",
+#             -1: "55011271",
+#             2: "58860488",
+#             1: "55011271",
+#             3: "60457274"}
+
+actor_map = {"S{}".format(idx): "s_0{}".format(idx) for idx in [1,5,6,7,8,9]}
+actor_map["S11"] = "s_11"
+camera_idx = "ca_0{}".format(view_camera_index)
+action_map = {action: "act_{}".format(str(int(i/2)+2).zfill(2)) for i, action in enumerate(label_action_name)}
+subaction = "subact_01" if "1" in specific_action else "subact_02"
+
+img_string = "{}_{}_{}_{}".format(actor_map[specific_subject], action_map[specific_action], subaction, camera_idx)
+# img_folder = os.path.join(img_root, )
+
+while True:
+
+    if frame_index == total_frame:
+        frame_index = 0
+    frame = np.zeros([frame_size, frame_size, 3])
+    if view_camera_index >= 0:
+        view_matrix = None
+        projection_matrix = specific_camera_config[view_camera_index].projection
+    else:
+        view_matrix = o_view_matrix
+        projection_matrix = o_projection_matrix
+
+    grid_vertices_project = grid_vertices @ (np.eye(3) if view_matrix is None else rorate_x_90[:3, :3].T)
+    grid_vertices_project = grid_vertices_project @ transformations.scale_matrix(650)[:3, :3].T
+    grid_vertices_project = projection_to_2d_plane(grid_vertices_project, projection_matrix, view_matrix,
+                                                   int(frame_size / 2)).reshape(-1, 4)
+
+    # draw line
+    for index, line in enumerate(grid_vertices_project):
+        cv2.line(frame, (line[0], line[1]), (line[2], line[3]), grid_color[index].tolist())
+
+    # draw camera
+    for camera_index, conf in enumerate(specific_camera_config):
+        if view_camera_index == camera_index:
+            continue
+        m_rt = transformations.identity_matrix()
+        r = np.array(conf.R, dtype=np.float32).T
+        m_rt[:-1, -1] = -r @ np.array(conf.t, dtype=np.float32).squeeze()
+        m_rt[:-1, :-1] = r
+
+        m_s = transformations.identity_matrix() * 200
+        m_s[3, 3] = 1
+
+        camera_vertices_convert = homogeneous_to_euclidean(
+            euclidean_to_homogeneous(camera_vertices) @ (
+                    (np.eye(4) if view_matrix is None else rorate_x_90) @ m_rt @ m_s).T)
+
+        camera_vertices_convert = projection_to_2d_plane(camera_vertices_convert, projection_matrix, view_matrix,
+                                                         int(frame_size / 2))
+        camera_vertices_convert = camera_vertices_convert.reshape(-1, 4)
+        for index, line in enumerate(camera_vertices_convert):
+            cv2.line(frame, (line[0], line[1]), (line[2], line[3]), (0, 153, 255), thickness=1)
+        cv2.putText(frame, camera_name[camera_index],
+                    (camera_vertices_convert[1, 0], camera_vertices_convert[1, 1] - 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255))
+
+    specific_3d_skeleton_project = specific_3d_skeleton[frame_index].reshape(-1, 3)
+
+    ax = plt.subplot(1, 1, 1, projection='3d')
+    show3Dpose(specific_3d_skeleton_project, ax, plot_dot=True, add_labels=False)
+    plt.tight_layout()
+    plt.savefig("tmp.JPG")
+    plt.clf()
+    img3d = cv2.resize(cv2.imread("tmp.JPG"), (720, 540))
+    cv2.imshow("3d", img3d)
+
+    img_path = os.path.join(img_root, img_string, img_string+"_"+str(frame_index+1).zfill(6)+".jpg")
+    origin_img = cv2.imread(img_path)
+    cv2.imshow("img_origin", origin_img)
+
+    # img_path = os.path.join(img_folder, cam_dict[view_camera_index], "img_" + str(frame_index+1).zfill(6) + ".jpg")
+    # try:
+    #     # origin_img = cv2.resize(cv2.imread(img_path), (720, 540))
+    #     origin_img = cv2.imread(img_path)
+    #     cv2.imshow("img_origin", origin_img)
+    # except:
+    #     pass
+
+    specific_3d_skeleton_project = specific_3d_skeleton_project @ (
+        np.eye(3) if view_matrix is None else rorate_x_90[:3, :3]).T
+    specific_3d_skeleton_project = specific_3d_skeleton_project @ np.eye(3, dtype=np.float32) * 1
+    specific_3d_skeleton_project = projection_to_2d_plane(specific_3d_skeleton_project, projection_matrix, view_matrix,
+                                                          int(frame_size / 2)).reshape(17, 2)
+
+    for c in human36m_connectivity_dict:
+        cv2.line(frame, (*specific_3d_skeleton_project[c[0]],), (*specific_3d_skeleton_project[c[1]],),
+                 (100, 155, 255), thickness=2)
+        cv2.circle(frame, (*specific_3d_skeleton_project[c[0]],), 3, (0, 0, 255), -1)
+        cv2.circle(frame, (*specific_3d_skeleton_project[c[1]],), 3, (0, 0, 255), -1)
+
+    frame_index += 1
+    # print(frame_index)
+    cv2.imshow(specific_action, frame)
+    out.write(np.uint8(frame))
+    frame = cv2.resize(frame, (720, 540))
+
+    # cv2.imshow("img_origin", origin_img)
+    # out_img = cv2.hconcat([origin_img,img3d,frame])
+    # out_img = np.concatenate((origin_img, cv2.resize(cv2.imread("tmp.JPG"), (720, 540)), frame), axis=0)
+    # cv2.imshow("result", out_img)
+    # cv2.waitKey(1)
+
+    # if cv2.waitKey(6) & 0xFF == ord('1'):
+    #     view_camera_index += 1
+    #     if view_camera_index == 4:
+    #         view_camera_index = -1
+    cv2.waitKey(1)
+
+cv2.destroyAllWindows()
